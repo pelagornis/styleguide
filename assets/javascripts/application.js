a806e13e252846fd39834c599db731bb981e5cc5
chilensis.dev <chilensis.dev@gmail.com>
2024-10-10 03:49:45 +0000

    Update application js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,14 +1,27 @@
-window.addEventListener('scroll', function() {
-    const footer = document.querySelector('.footer');
-    const dock = this.document.querySelector('.dock');
-    const footerHeight = footer.offsetHeight;
-    const dockHeight = dock.offsetHeight;
-    const contentHeight = document.body.scrollHeight;
-    const scrollPosition = window.scrollY + window.innerHeight;
-  
-    if (scrollPosition >= contentHeight - footerHeight - dockHeight) {
-        dock.style.position = 'relative';
-    } else {
-        dock.style.position = 'fixed';
+function attachEvent(selector, event, fn) {
+    const matches = typeof selector === 'string' ? document.querySelectorAll(selector) : selector;
+    if (matches && matches.length) {
+        matches.forEach((elem) => {
+            elem.addEventListener(event, (e) => fn(e, elem), false);
+        });
     }
-});
+}
+
+const onLoad = function() {    
+    attachEvent([document], 'scroll', function() {
+        const footer = document.querySelector('.footer');
+        const dock = this.document.querySelector('.dock');
+        const footerHeight = footer.offsetHeight;
+        const dockHeight = dock.offsetHeight;
+        const contentHeight = document.body.scrollHeight;
+        const scrollPosition = window.scrollY + window.innerHeight;
+      
+        if (scrollPosition >= contentHeight - footerHeight - dockHeight) {
+            dock.style.position = 'relative';
+        } else {
+            dock.style.position = 'fixed';
+        }
+    });
+};
+
+window.onload = onLoad;
